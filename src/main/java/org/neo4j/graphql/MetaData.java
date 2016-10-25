cc4528dbdcc033cad39dcd82b5d126b713fe03ff
Michael Hunger <github@jexp.de>
2016-10-25 05:19:14 +0200

    Added first version of relationship support
    * collect relationship metadata from sampled nodes
    * decide on result type (list, object) by degree
    * generate & run query with optional matches and projections

--- src/main/java/org/neo4j/graphql/MetaData.java
+++ src/main/java/org/neo4j/graphql/MetaData.java
@@ -6,9 +6,11 @@ import graphql.schema.*;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.schema.IndexDefinition;
+import org.neo4j.helpers.collection.Iterables;
 import org.neo4j.helpers.collection.Iterators;
 
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import static graphql.Scalars.GraphQLString;
@@ -21,18 +23,67 @@ import static graphql.schema.GraphQLObjectType.newObject;
  * @since 24.10.16
  */
 public class MetaData {
+    public static final int DENSE_NODE = 50;
     private static Map<String, MetaData> allTypes = new LinkedHashMap<>();
     private String type;
     private Set<String> ids = new LinkedHashSet<>();
     private Set<String> indexed = new LinkedHashSet<>();
     private Map<String, Class<?>> properties = new LinkedHashMap<>();
     private Set<String> labels = new LinkedHashSet<>();
-    private Map<String, String> relationships = new LinkedHashMap<>();
+    Map<String, RelationshipInfo> relationships = new LinkedHashMap<>();
 
+    static class RelationshipInfo {
+        String label;
+        String type;
+        boolean multi;
+        boolean out;
+
+        static RelationshipInfo from(String type, String label, boolean multi, boolean out) {
+            RelationshipInfo info = new RelationshipInfo();
+            info.label = label;
+            info.type = type;
+            info.multi |= multi;
+            info.out = out;
+            return info;
+        }
+
+        RelationshipInfo update(boolean multi) {
+            this.multi |= multi;
+            return this;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            RelationshipInfo that = (RelationshipInfo) o;
+
+            return out == that.out && label.equals(that.label) && type.equals(that.type);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = label.hashCode();
+            result = 31 * result + type.hashCode();
+            result = 31 * result + (out ? 1 : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "RelationshipInfo{" +
+                    "label='" + label + '\'' +
+                    ", type='" + type + '\'' +
+                    ", multi=" + multi +
+                    ", out=" + out +
+                    '}';
+        }
+    }
     public MetaData(GraphDatabaseService db, Label label) {
         this.type = label.name();
         inspectIndexes(db, label);
-        sampleProperties(db, label);
+        sampleNodes(db, label);
     }
 
     @Override
@@ -95,6 +146,7 @@ public class MetaData {
                 })
                 .name(type)
                 .description(type + "-Label");
+        // it uses the interface types for checking against parent-types of fields, which is weird
         builder = addProperties(builder, ids, indexed); // mostly id, indexed, not sure about others
         return builder.build();
     }
@@ -120,9 +172,19 @@ public class MetaData {
 
 // todo relationships, labels etc.
 
-        builder = addInterfaces(builder);
-        builder = addProperties(builder); // mostly id, indexed, not sure about others
+// something is off with rule-checking probably interface names conflicting with object names
+//        builder = addInterfaces(builder);
+        builder = addProperties(builder);
+        builder = addRelationships(builder);
         return builder.build();
+    }
+
+    private GraphQLObjectType.Builder addRelationships(GraphQLObjectType.Builder builder) {
+        for (Map.Entry<String, RelationshipInfo> entry : relationships.entrySet()) {
+            RelationshipInfo info = entry.getValue();
+            builder = builder.field(newReferenceField(entry.getKey(), info.label,info.multi));
+        }
+        return builder;
     }
 
     private GraphQLObjectType.Builder addProperties(GraphQLObjectType.Builder builder) {
@@ -134,6 +196,21 @@ public class MetaData {
         return builder;
     }
 
+    private GraphQLFieldDefinition newReferenceField(String name, String label, boolean multi) {
+        return newFieldDefinition()
+                .name(name)
+/*
+                .dataFetcher((env) -> {
+                    return ((List<Map<String,Object>>)env.getSource()).stream()
+                            .flatMap( (row) ->
+                                    env.getFields().stream().map(f -> row.get(f.getName())))
+                            .collect(Collectors.toList());
+                })
+*/
+                .description(this.type+" "+name+" "+label)
+                .type(multi ? new GraphQLList(new GraphQLTypeReference(label)) : new GraphQLTypeReference(label))
+                .build();
+    }
     private GraphQLFieldDefinition newField(String name, Class<?> type) {
         return newFieldDefinition()
                 .name(name)
@@ -163,7 +240,7 @@ public class MetaData {
         return new MetaData(db, label);
     }
 
-    private void sampleProperties(GraphDatabaseService db, Label label) {
+    private void sampleNodes(GraphDatabaseService db, Label label) {
         int count = 10;
         ResourceIterator<Node> nodes = db.findNodes(label);
         Map<String, Object> values = new LinkedHashMap<>();
@@ -171,9 +248,46 @@ public class MetaData {
             Node node = nodes.next();
             for (Label l : node.getLabels()) labels.add(l.name());
             values.putAll(node.getAllProperties());
+            sampleRelationships(node);
         }
         values.forEach((k, v) -> properties.put(k, v.getClass()));
         labels.remove(type);
+    }
+
+    private void sampleRelationships(Node node) {
+        boolean dense = node.getDegree() > DENSE_NODE;
+        for (RelationshipType type : node.getRelationshipTypes()) {
+            Iterator<Relationship> itOut = node.getRelationships(Direction.OUTGOING, type).iterator();
+            Relationship out = Iterators.firstOrNull(itOut);
+            String typeName = type.name();
+            if (out != null) {
+                if (!dense || node.getDegree(type,Direction.OUTGOING)<DENSE_NODE) {
+                    String outName = typeName + "_";
+                    labelsFor(out.getEndNode(),(label)-> this.relationships.compute(outName+label, (key,info) ->
+                            info == null ?
+                                    RelationshipInfo.from(typeName,label,itOut.hasNext(), true) :
+                                    info.update(itOut.hasNext())));
+                }
+            }
+            Iterator<Relationship> itIn = node.getRelationships(Direction.INCOMING, type).iterator();
+            Relationship in = Iterators.firstOrNull(itIn);
+            if (in != null) {
+                if (!dense || node.getDegree(type,Direction.INCOMING)<DENSE_NODE) {
+                    String inName = "_"+typeName;
+                    labelsFor(in.getStartNode(),(label)-> this.relationships.compute(label+inName,(key,info) ->
+                            info == null ?
+                                    RelationshipInfo.from(typeName,label,itIn.hasNext(), false) :
+                                    info.update(itIn.hasNext())));
+                }
+            }
+        }
+    }
+
+
+    private void labelsFor(Node node, Consumer<String> consumer) {
+        for (Label label : node.getLabels()) {
+            consumer.accept(label.name());
+        }
     }
 
     private static GraphQLOutputType graphQlOutType(Class<?> type) {
@@ -228,29 +342,74 @@ public class MetaData {
     }
 
     private static String generateQueryForField(Field field) {
-        String variable = "n";
-        String query = String.format("MATCH (%s:`%s`) ",variable,field.getName());
-        if (!field.getArguments().isEmpty()) {
-            query += " WHERE ";
-            Iterator<Argument> args = field.getArguments().iterator();
-            while (args.hasNext()) {
-                Argument argument = args.next();
-                query += String.format("%s.`%s` = ", variable, argument.getName());
-                Value value = argument.getValue();
-                if (value instanceof VariableReference) query += String.format("{`%s`}", ((VariableReference) value).getName());
-                // todo turn into parameters
-                if (value instanceof IntValue) query += ((IntValue)value).getValue();
-                if (value instanceof StringValue) query += "\""+((StringValue)value).getValue()+"\"";
-                if (args.hasNext()) query += " AND ";
+        String name = field.getName();
+        String variable = name;
+        MetaData metaData = allTypes.get(name);
+
+        String query = String.format("MATCH (%s:`%s`) \n",variable, name);
+        query += addWhere(field, variable);
+        for (Selection selection : field.getSelectionSet().getSelections()) {
+            if (selection instanceof Field) {
+                String fieldName = ((Field) selection).getName();
+                RelationshipInfo info = metaData.relationships.get(fieldName);
+                if (info != null) {
+                    query += String.format(" OPTIONAL MATCH (`%s`)%s-[:`%s`]-%s(`%s`:`%s`) \n",variable, !info.out ? "<" : "", info.type, info.out ? ">" : "", fieldName, info.label);
+// todo handle conditions on related elements after optional match
+//TODO                    query += addWhere(field, variable);
+                }
             }
         }
         query += " RETURN ";
-        Iterator<Selection> it = field.getSelectionSet().getSelections().iterator();
+        query += selectionFields(field, variable, metaData,false);
+        System.out.println("query = " + query);
+        return query;
+    }
+
+    private static String addWhere(Field field, String variable) {
+        if (field.getArguments().isEmpty()) {
+            return "";
+        }
+        String query = " WHERE ";
+        Iterator<Argument> args = field.getArguments().iterator();
+        while (args.hasNext()) {
+            Argument argument = args.next();
+            query += String.format("%s.`%s` = ", variable, argument.getName());
+            Value value = argument.getValue();
+            if (value instanceof VariableReference) query += String.format("{`%s`}", ((VariableReference) value).getName());
+            // todo turn into parameters
+            if (value instanceof IntValue) query += ((IntValue)value).getValue();
+            if (value instanceof StringValue) query += "\""+((StringValue)value).getValue()+"\"";
+            if (args.hasNext()) query += " AND \n";
+        }
+        return query;
+    }
+
+    private static String selectionFields(Field field, String variable, MetaData metaData, boolean prefix) {
+        SelectionSet selectionSet = field.getSelectionSet();
+        if (selectionSet==null) return "";
+
+        Iterator<Selection> it = selectionSet.getSelections().iterator();
+        String query = "";
         while (it.hasNext()) {
             Field f = (Field) it.next();
-            String alias = f.getAlias() != null ? f.getAlias() : f.getName();
-            query += String.format("%s.`%s` AS `%s`", variable, f.getName(), alias);
-            if (it.hasNext()) query += ",";
+            String fieldName = f.getName();
+            String alias = f.getAlias() != null ? f.getAlias() : fieldName;
+            RelationshipInfo info = metaData.relationships.get(fieldName); // todo correct medatadata of
+            if (prefix) {
+                query += alias + ": ";
+            }
+            if (info == null) {
+                query += String.format("`%s`.`%s`", variable, fieldName);
+            } else {
+                if (f.getSelectionSet() != null ) {
+                    String subFields = selectionFields(f, f.getName(), allTypes.get(info.label), true);
+                    query += String.format(info.multi ? "collect({%s})" : "{%s}", subFields);
+                }
+            }
+            if (!prefix) {
+                query += " AS " + alias;
+            }
+            if (it.hasNext()) query += ",\n";
         }
         return query;
     }
--- src/test/java/org/neo4j/graphql/MetaDataTest.java
+++ src/test/java/org/neo4j/graphql/MetaDataTest.java
@@ -8,12 +8,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.neo4j.graphdb.Label.label;
 import static org.neo4j.helpers.collection.MapUtil.map;
 
 /**
@@ -28,7 +30,7 @@ public class MetaDataTest {
     @Before
     public void setUp() throws Exception {
         db = new TestGraphDatabaseFactory().newImpermanentDatabase();
-        db.execute("UNWIND range(1,5) as id CREATE (:User:Person {name:'John '+id, id:id, age:id})").close();
+        db.execute("CREATE (earth:Location {name:'Earth'}) WITH earth UNWIND range(1,5) as id CREATE (:User:Person {name:'John '+id, id:id, age:id})-[:LIVES_ON]->(earth)").close();
         GraphQLSchema graphQLSchema = MetaData.buildSchema(db);
         graphql = new GraphQL(graphQLSchema);
     }
@@ -39,9 +41,72 @@ public class MetaDataTest {
     }
 
     @Test
+    public void sampleRelationships() throws Exception {
+        try (Transaction tx = db.beginTx()) {
+            MetaData person = MetaData.from(db, label("Person"));
+            MetaData.RelationshipInfo LIVES_ON_Location = MetaData.RelationshipInfo.from("LIVES_ON", "Location", false, true);
+            assertEquals(map("LIVES_ON_Location", LIVES_ON_Location), person.relationships);
+            MetaData location = MetaData.from(db, label("Location"));
+            MetaData.RelationshipInfo Person_LIVES_ON = MetaData.RelationshipInfo.from("LIVES_ON", "Person", true, false);
+            MetaData.RelationshipInfo User_LIVES_ON = MetaData.RelationshipInfo.from("LIVES_ON", "User", true, false);
+            assertEquals(map("Person_LIVES_ON", Person_LIVES_ON,"User_LIVES_ON", User_LIVES_ON), location.relationships);
+            tx.success();
+        }
+    }
+
+    @Test
     public void allUsersQuery() throws Exception {
         Map<String, List<Map>> result = executeQuery("query UserQuery { User {id,name,age} User {age,name}}", map());
         assertEquals(2*5, result.get("User").size());
+    }
+    @Test
+    public void allLocationsQuery() throws Exception {
+        Map<String, List<Map>> result = executeQuery("query LocationQuery { Location {name} }", map());
+        assertEquals(1, result.get("Location").size());
+        assertEquals("Earth", result.get("Location").get(0).get("name"));
+    }
+
+    @Test
+    public void singleUserWithLocationQuery() throws Exception {
+        Map<String, List<Map>> result = executeQuery("query UserWithLocationQuery { User(id:3) {name,LIVES_ON_Location {name} } }", map());
+        List<Map> users = result.get("User");
+        assertEquals(1, users.size());
+        Map user = users.get(0);
+        assertEquals("John 3", user.get("name"));
+        Map location = (Map) user.get("LIVES_ON_Location");
+        assertEquals("Earth", location.get("name"));
+    }
+    @Test
+    public void usersWithLocationQuery() throws Exception {
+        Map<String, List<Map>> result = executeQuery("query UserWithLocationQuery { User {name,LIVES_ON_Location {name} } }", map());
+        List<Map> users = result.get("User");
+        assertEquals(5, users.size());
+        Map user = users.get(0);
+        assertEquals("John 1", user.get("name"));
+        Map location = (Map) user.get("LIVES_ON_Location");
+        assertEquals("Earth", location.get("name"));
+    }
+    @Test
+    public void locationWithUsersQuery() throws Exception {
+        Map<String, List<Map>> result = executeQuery("query LocationWithUserQuery { Location {name, User_LIVES_ON {name,age} } }", map());
+        List<Map> locations = result.get("Location");
+        assertEquals(1, locations.size());
+        Map location = locations.get(0);
+        assertEquals("Earth", location.get("name"));
+        List<Map> people = (List<Map>) location.get("User_LIVES_ON");
+        assertEquals(5, people.size());
+        people.forEach((p) -> assertEquals(true, p.get("name").toString().startsWith("John")));
+    }
+    @Test
+    public void locationWithPeopleQuery() throws Exception {
+        Map<String, List<Map>> result = executeQuery("query LocationWithPersonQuery { Location {name, Person_LIVES_ON {name,age} } }", map());
+        List<Map> locations = result.get("Location");
+        assertEquals(1, locations.size());
+        Map location = locations.get(0);
+        assertEquals("Earth", location.get("name"));
+        List<Map> people = (List<Map>) location.get("Person_LIVES_ON");
+        assertEquals(5, people.size());
+        people.forEach((p) -> assertEquals(true, p.get("name").toString().startsWith("John")));
     }
     @Test
     public void oneUserParameterQuery() throws Exception {
