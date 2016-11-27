69448bb7fed9202d2581313f8781a937015a8c0d
Michael Hunger <github@jexp.de>
2016-11-27 20:46:30 +0100

    Made server test run faster by creating the test-server only once per test.

--- src/test/java/org/neo4j/graphql/GraphQLResourceTest.java
+++ src/test/java/org/neo4j/graphql/GraphQLResourceTest.java
@@ -1,19 +1,20 @@
 package org.neo4j.graphql;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Result;
-import org.neo4j.harness.junit.Neo4jRule;
+import org.neo4j.harness.ServerControls;
+import org.neo4j.harness.TestServerBuilders;
 import org.neo4j.test.server.HTTP;
 
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static org.neo4j.helpers.collection.MapUtil.map;
 
 /**
@@ -22,16 +23,27 @@ import static org.neo4j.helpers.collection.MapUtil.map;
  */
 public class GraphQLResourceTest {
 
-    @Rule
-    public Neo4jRule neo4j = new Neo4jRule()
-            .withExtension("/graphql", GraphQLResource.class)
-            .withProcedure(GraphQLProcedure.class)
-            .withFixture("CREATE (:Person {name:'Kevin Bacon',born:1958})-[:ACTED_IN]->(:Movie {title:'Apollo 13',released:1995}),(:Person {name:'Meg Ryan',born:1961})");
+    private static ServerControls neo4j;
+    private static URL serverURI;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        neo4j = TestServerBuilders
+                .newInProcessBuilder()
+                .withExtension("/graphql", GraphQLResource.class)
+                .withProcedure(GraphQLProcedure.class)
+                .withFixture("CREATE (:Person {name:'Kevin Bacon',born:1958})-[:ACTED_IN]->(:Movie {title:'Apollo 13',released:1995}),(:Person {name:'Meg Ryan',born:1961})")
+                .newServer();
+        serverURI = new URL(neo4j.httpURI().toURL(), "graphql/");
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        neo4j.close();
+    }
 
     @Test
     public void allPeople() throws Exception {
-        URL serverURI = new URL(neo4j.httpURI().toURL(), "graphql/");
-
         HTTP.Response response = HTTP.POST(serverURI.toString(), map("query", "query AllPeopleQuery { Person {name,born} }"));
 
         assertEquals(200, response.status());
@@ -49,8 +61,6 @@ public class GraphQLResourceTest {
 
     @Test
     public void personByYear() throws Exception {
-        URL serverURI = new URL(neo4j.httpURI().toURL(), "graphql/");
-
         HTTP.Response response = HTTP.POST(serverURI.toString(), map("query", "query AllPeopleQuery { Person(born:1961) {name,born} }"));
 
         assertEquals(200, response.status());
@@ -65,8 +75,6 @@ public class GraphQLResourceTest {
     }
     @Test
     public void personByNameParameter() throws Exception {
-        URL serverURI = new URL(neo4j.httpURI().toURL(), "graphql/");
-
         HTTP.Response response = HTTP.POST(serverURI.toString(), map("query", "query AllPeopleQuery($name:String!) { Person(name:$name) {name,born} }","variables",map("name","Meg Ryan")));
 
         assertEquals(200, response.status());
@@ -83,7 +91,7 @@ public class GraphQLResourceTest {
     @Test
     public void testProcedureCall() throws Exception {
         String query = "query AllPeopleQuery { Person(born:1961) {name,born} }";
-        GraphDatabaseService db = neo4j.getGraphDatabaseService();
+        GraphDatabaseService db = neo4j.graph();
         Result result = db.execute("CALL graphql.execute({query},{})",map("query",query));
         assertEquals(true,result.hasNext());
         Map<String, Object> row = result.next();
@@ -97,7 +105,7 @@ public class GraphQLResourceTest {
     @Test
     public void testProcedureCallFail() throws Exception {
         try {
-            GraphDatabaseService db = neo4j.getGraphDatabaseService();
+            GraphDatabaseService db = neo4j.graph();
             Result result = db.execute("CALL graphql.execute('foo',{})");
             fail("Procedure call should fail");
         } catch(Exception e) {
